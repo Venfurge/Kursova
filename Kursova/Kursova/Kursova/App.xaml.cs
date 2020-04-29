e3011889a85617b99bce09af6592ebcf41221008
ZheniaB <Venfurge@users.noreply.github.com>
2020-04-29 11:02:30 +0300

    Initialize commit

---
+++ Kursova/Kursova/Kursova/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using Kursova.ViewModels;
+using Kursova.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Kursova
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
